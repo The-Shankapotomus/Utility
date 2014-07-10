cf7383eb74334f7527974b040b0ab43ae9937c9f
The-Shankapotomus <jshank8102@gmail.com>
2014-07-10 14:08:31 -0400

    Setup Utility egg v.0.1.0

---
+++ Utility/Utility/getconfig.py
@@ -0,0 +1,38 @@
+import os
+
+class Utility:
+
+    def __init__(self, filePath):
+
+        #gets path of file running currently
+        self.currentPath = filePath
+
+        #a list of sub directories that should exist within root 
+        self.subDirs = ('\game', '\http', '\scram', '\test', '\twisted')
+
+    #check to see if the path contains a subdirectory extension, if so remove it
+    def check(self):
+
+        for sub in self.subDirs:
+
+            if sub in self.currentPath:
+
+                cdir = self.currentPath.split(sub, 1)[0]
+                
+        return cdir
+
+    #find the file within the working directory
+    def find(self, name, path):
+
+        for root, dirs, files in os.walk(path):
+
+                  if name in files:
+
+                      return os.path.join(root, name)
+
+    #implement this method to get the file you need find
+    def getPath(self, fileName):
+                  
+        path = self.find(fileName, self.check())
+
+        return path
---
+++ Utility/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'Utility',
+      version = '0.1.0',
+      description = 'Utility Module',
+      author = 'Joshua Shank',
+      author_email = 'joshua.shank@altamiracorp.com',
+      packages = ['Utility'],
+     )
